6883a7ff34aa5b7e38a921c5b0847e3dcc003e58
unknown <jkl2554@gmail.com>
2023-03-02 18:23:26 +0900

    update main

--- deploy.ps1
+++ deploy.ps1
@@ -4,22 +4,22 @@ if($(git status -s)){
 }
 
 Write-Output "Deleting old docsation"
-Remove-Item -Recurse -Force docs/* -Exclude "CNAME"
-mkdir docs
+Remove-Item -Recurse -Force public/* -Exclude "CNAME"
+# mkdir public
 git worktree prune
-Remove-Item -Recurse -Force .git/worktrees/docs/
+Remove-Item -Recurse -Force .git/worktrees/public/
 
-Write-Output "Checking out gh-pages branch into docs"
-git worktree add -B gh-pages docs origin/gh-pages
+Write-Output "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
 
 Write-Output "Removing existing files"
-Remove-Item -Recurse -Force docs/* -Exclude "CNAME"
+Remove-Item -Recurse -Force public/* -Exclude "CNAME"
 
 Write-Output "Generating site"
-$env:HUGO_ENV="production" && hugo -t github-style -d docs
+$env:HUGO_ENV="production" && hugo -t github-style
 
 Write-Output "Updating gh-pages branch"
-Set-Location docs && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+Set-Location public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 Write-Output "Pushing to github"
 git push --all
